e09f61330c0a12143aa4861a7e45dfa5f77ea27e
brianramaswami <brianramaswami@yahoo.com>
2018-01-30 11:48:55 -0800

    adding cpsc326 with first hw

---
+++ cpsc326/HW1/hw1.cpp
@@ -0,0 +1,39 @@
+#Brian Ramaswami
+#1/25/18
+#hw1
+
+
+
+#1
+var a =  [2,3,4,5];
+if a[3] == 4 then
+    println("index 3 is 5");
+elseif a[3] == 2 then
+    println("index 3 is 2");
+else
+    println("index 3 is not equal to 5 or 2");
+
+#2
+var b = "Hello World!!!";
+println(b);
+
+#3
+x = readint("Enter an number: ");
+y = readint("Enter another number: ");
+if x + y == y = x then
+    println("True");
+else 
+    println("False");
+
+
+#4
+k = 0;
+i = 100;
+x = true; 
+while x == true do
+    while k < i  and k > 50 do
+    println(k);
+    k++;
+    end;
+i= i +1;
+end;
